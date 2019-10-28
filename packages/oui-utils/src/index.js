39c0c51627be6cfd0f5be90321237a40801a9ae6
Martin Barri <maeertin@gmail.com>
2019-10-28 14:32:07 +0100

    [Utils] Add deepmerge util function.

--- packages/oui-utils/src/index.js
+++ packages/oui-utils/src/index.js
@@ -1,5 +1,6 @@
 export { default as capitalize } from './capitalize'
 export { default as clamp } from './clamp'
+export { default as deepmerge } from './deepmerge'
 export { default as lerp } from './lerp'
 export { default as map } from './map'
 export { default as normalize } from './normalize'
