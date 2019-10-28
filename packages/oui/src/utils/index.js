8e291b0a6fb887cf67bcc4cff7d0554d979a8c46
Martin Barri <maeertin@gmail.com>
2019-10-28 18:19:47 +0100

    [utils] Move component related helpers from @oakwood/oui-utils to @oakwood/oui/utils

---
+++ packages/oui/src/utils/index.js
@@ -0,0 +1,5 @@
+export { default as capitalize } from './capitalize'
+export { default as clamp } from './clamp'
+export { default as lerp } from './lerp'
+export { default as map } from './map'
+export { default as normalize } from './normalize'
