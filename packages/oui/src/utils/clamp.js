32c93281b129153213d9431b7465bab1951eb9bc
Martin Barri <maeertin@gmail.com>
2019-11-14 09:55:07 +0100

    [utils] Remove all utils

--- packages/oui/src/utils/clamp.js
+++
@@ -1,5 +0,0 @@
-function clamp(value, min, max) {
-  return Math.min(Math.max(value, min), max)
-}
-
-export default clamp
--- packages/oui/src/utils/normalize.js
+++
@@ -1,7 +0,0 @@
-import map from './map'
-
-function normalize(value, x, y) {
-  return map(value, x, y, 0, 1)
-}
-
-export default normalize
