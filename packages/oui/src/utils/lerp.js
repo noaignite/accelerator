1a15bef14d8cf5438d9140d0589e1cf92d7f34e5
Martin Barri <maeertin@gmail.com>
2019-11-14 09:55:07 +0100

    [utils] Remove all utils

--- packages/oui/src/utils/lerp.js
+++
@@ -1,5 +0,0 @@
-function lerp(start, end, amount) {
-  return (1 - amount) * start + amount * end
-}
-
-export default lerp
--- packages/oui/src/utils/map.js
+++
@@ -1,5 +0,0 @@
-function map(value, inMin, inMax, outMin, outMax) {
-  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
-}
-
-export default map
