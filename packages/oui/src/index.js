c8d48f14f0ced386e1d41d3433b38dc450688900
Martin Barri <maeertin@gmail.com>
2019-11-26 15:27:14 +0100

    [index.js] Update export pattern

--- packages/oui/src/index.js
+++ packages/oui/src/index.js
@@ -1,3 +1,10 @@
+/* eslint-disable import/export */
+
 export { default as Media } from './Media'
+export * from './Media'
+
 export { default as MediaLoader } from './MediaLoader'
+export * from './MediaLoader'
+
 export { default as ScrollProgress } from './ScrollProgress'
+export * from './ScrollProgress'
