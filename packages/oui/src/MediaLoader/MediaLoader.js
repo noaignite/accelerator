e2b40d6aa7c5f6c2f95cd64103bccef31dc29461
Martin Barri <maeertin@gmail.com>
2019-11-26 15:28:01 +0100

    [MediaLoader] Use clsx in favor of classnames

--- packages/oui/src/MediaLoader/MediaLoader.js
+++ packages/oui/src/MediaLoader/MediaLoader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
+import classnames from 'clsx'
 import imagesLoaded from 'imagesloaded'
 import { InView } from 'react-intersection-observer'
 import withStyles from '@material-ui/core/styles/withStyles'
