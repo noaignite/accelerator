297f11e134b8984fc0670e4a8bf02d0256075a81
Martin Barri <maeertin@gmail.com>
2020-04-24 11:03:32 +0200

    [docs] Configure @storybook/addon-a11y and add story sorting

--- docs/.storybook/config.js
+++ docs/.storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { addDecorator, configure } from '@storybook/react'
+import { addDecorator, addParameters, configure } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
 import MuiThemeProvider from '@material-ui/styles/ThemeProvider'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -12,6 +13,18 @@ function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
+addParameters({
+  options: {
+    storySort: (a, b) => {
+      if (a[1].kind === b[1].kind) {
+        return 0
+      }
+
+      return a[1].id.localeCompare(b[1].id, { numeric: true })
+    },
+  },
+})
+
 addDecorator(story => {
   const content = story()
   return (
@@ -22,4 +35,6 @@ addDecorator(story => {
   )
 })
 addDecorator(withKnobs)
+addDecorator(withA11y)
+
 configure(loadStories, module)
