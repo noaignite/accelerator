6e807f3794120a61579c452647d72a11cb365c1b
Martin Barri <maeertin@gmail.com>
2020-04-24 11:57:11 +0200

    [BackgroundMedia] Add new component for creating stylish background image effects

---
+++ packages/oui/src/BackgroundMedia/BackgroundMedia.js
@@ -0,0 +1,74 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'clsx'
+import { capitalize } from '@material-ui/core/utils'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+export const styles = () => {
+  const absolute = {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  }
+
+  return {
+    root: {
+      ...absolute,
+      clipPath: 'inset(0 0 0 0)', // Use `clipPath` as `overflow="hidden"` breaks `position="sticky"`
+      WebkitClipPath: 'inset(0 0 0 0)',
+    },
+    container: {
+      ...absolute,
+    },
+    containerFixed: {
+      position: 'fixed',
+    },
+    containerSticky: {
+      bottom: '-100%',
+    },
+    wrapper: {
+      ...absolute,
+    },
+    wrapperFixed: {},
+    wrapperSticky: {
+      position: 'sticky',
+      bottom: 'auto',
+      height: '50%',
+    },
+  }
+}
+
+const BackgroundMedia = React.forwardRef(function BackgroundMedia(props, ref) {
+  const { attachment = 'static', children, classes, className, ...other } = props
+
+  return (
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+      <div
+        className={classnames(classes.container, {
+          [classes[`container${capitalize(attachment)}`]]: attachment !== 'static',
+        })}
+      >
+        <div
+          className={classnames(classes.wrapper, {
+            [classes[`wrapper${capitalize(attachment)}`]]: attachment !== 'static',
+          })}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+})
+
+BackgroundMedia.propTypes = {
+  attachment: PropTypes.oneOf(['static', 'fixed', 'sticky']),
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+}
+
+BackgroundMedia.uiName = 'BackgroundMedia'
+
+export default withStyles(styles)(BackgroundMedia)
