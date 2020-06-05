bb9abc196d4b021b2af9d53360ac32b4dc1af7c8
Martin Barri <maeertin@gmail.com>
2020-06-05 17:21:37 +0200

    [MediaLoader] Fix bug where loaded callback would run even after unmount. Now conditionally transitions either children or placeholder and no longer both. Update prop interface. Performance enhancements.

--- packages/oui/src/MediaLoader/MediaLoader.js
+++ packages/oui/src/MediaLoader/MediaLoader.js
@@ -6,6 +6,7 @@ import classnames from 'clsx'
 import mediaLoaded from '@maeertin/medialoaded'
 import { InView } from 'react-intersection-observer'
 import { elementAcceptingRef } from '@material-ui/utils'
+import { useControlled } from '@material-ui/core/utils'
 import withStyles from '@material-ui/styles/withStyles'
 import Fade from '@material-ui/core/Fade'
 import AspectRatio from '../AspectRatio'
@@ -20,119 +21,169 @@ export const styles = {
     width: '100%',
     height: '100%',
   },
-  media: {},
+  placeholder: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    // ⚠️ object-fit is not supported by IE 11.
+    objectFit: 'cover',
+  },
 }
 
 const MediaLoader = React.forwardRef(function MediaLoader(props, ref) {
   const {
-    children,
+    children: childrenProp,
     classes,
     className,
-    component = 'div',
-    height,
+    in: inProp,
     lazy,
-    onLoad,
-    onRender,
-    onReveal,
-    placeholder,
-    PlaceholderTransitionComponent = Fade,
-    PlaceholderTransitionProps,
+    onEnter,
+    onEntered,
+    onEntering,
+    onLoaded,
+    placeholder: placeholderProp,
     rootMargin,
     TransitionComponent = Fade,
-    transitionDuration = 1000,
+    transitionDuration = 750,
     TransitionProps,
-    width,
     ...other
   } = props
 
+  const mediaRef = React.useRef(null)
+
   const [shouldRender, setShouldRender] = React.useState(!lazy)
-  const [shouldReveal, setShouldReveal] = React.useState(!rootMargin)
+  const [shouldReveal, setShouldReveal] = useControlled({
+    controlled: inProp,
+    default: !rootMargin,
+    name: 'MediaLoader',
+    state: 'shouldReveal',
+  })
   const [loaded, setLoaded] = React.useState(false)
 
-  const handleRender = React.useCallback(
-    (inView, entry) => {
-      if (inView) {
-        setShouldRender(inView)
-
-        if (onRender) {
-          onRender(entry)
-        }
-      }
-    },
-    [onRender],
-  )
-
-  const handleReveal = React.useCallback(
-    (inView, entry) => {
-      if (inView) {
-        setShouldReveal(inView)
-
-        if (onReveal) {
-          onReveal(entry)
-        }
-      }
-    },
-    [onReveal],
-  )
-
-  const handleLoad = React.useCallback(
-    (instance) => {
-      setLoaded(true)
-
-      if (onLoad) {
-        onLoad(instance)
-      }
-    },
-    [onLoad],
-  )
-
-  const handleMediaRef = (node) => {
-    if (node && !loaded) {
-      mediaLoaded(node, handleLoad)
+  const handleRenderIntersectionChange = React.useCallback((inView) => {
+    if (inView) {
+      setShouldRender(true)
     }
+  }, [])
+
+  const handleRevealIntersectionChange = React.useCallback(
+    (inView) => {
+      if (inView) {
+        setShouldReveal(true)
+      }
+    },
+    [setShouldReveal],
+  )
+
+  const handleLoaded = React.useCallback(
+    (instance) => {
+      if (mediaRef.current) {
+        setLoaded(true)
+
+        if (onLoaded) {
+          onLoaded(instance)
+        }
+      }
+    },
+    [onLoaded],
+  )
+
+  const handleMediaRef = React.useCallback(
+    (node) => {
+      mediaRef.current = node
+
+      if (node) {
+        mediaLoaded(node, handleLoaded)
+      }
+    },
+    [handleLoaded],
+  )
+
+  // Invert callback direction to simulate `children` still being transitioned.
+  const handleExit = React.useCallback(() => {
+    if (onEnter) {
+      onEnter(mediaRef.current)
+    }
+  }, [onEnter])
+
+  // Invert callback direction to simulate `children` still being transitioned.
+  const handleExited = React.useCallback(() => {
+    if (onEntered) {
+      onEntered(mediaRef.current)
+    }
+  }, [onEntered])
+
+  // Invert callback direction to simulate `children` still being transitioned.
+  const handleExiting = React.useCallback(() => {
+    if (onEntering) {
+      onEntering(mediaRef.current)
+    }
+  }, [onEntering])
+
+  const inState = loaded && shouldReveal
+
+  let placeholder = null
+  if (placeholderProp && React.isValidElement(placeholderProp)) {
+    placeholder = (
+      <TransitionComponent
+        className={classnames(classes.placeholder, placeholderProp.props.className)}
+        in={!inState}
+        onExit={handleExit}
+        onExited={handleExited}
+        onExiting={handleExiting}
+        timeout={transitionDuration}
+        appear={false} // Don't transition `placeholder` on mount.
+        unmountOnExit
+        {...TransitionProps}
+      >
+        {placeholderProp}
+      </TransitionComponent>
+    )
   }
 
-  const inProp = shouldReveal && loaded
+  let children = null
+  if (childrenProp && React.isValidElement(childrenProp)) {
+    children = React.cloneElement(childrenProp, { ref: handleMediaRef })
 
-  return (
-    <AspectRatio
-      className={classnames(classes.root, className)}
-      component={component}
-      height={height}
-      width={width}
-      ref={ref}
-      {...other}
-    >
-      {!shouldRender && (
-        <InView className={classes.bounds} onChange={handleRender} rootMargin="2000px 0px" />
-      )}
-      {!shouldReveal && (
-        <InView className={classes.bounds} onChange={handleReveal} rootMargin={rootMargin} />
-      )}
-
-      {shouldRender && (
+    if (!placeholder) {
+      children = (
         <TransitionComponent
-          className={classnames(classes.media, children.props.className)}
+          in={inState}
+          onEnter={onEnter}
+          onEntered={onEntered}
+          onEntering={onEntering}
           timeout={transitionDuration}
-          ref={handleMediaRef}
-          in={inProp}
           {...TransitionProps}
         >
           {children}
         </TransitionComponent>
+      )
+    }
+  }
+
+  return (
+    <AspectRatio className={classnames(classes.root, className)} ref={ref} {...other}>
+      {!shouldRender && (
+        <InView
+          className={classes.bounds}
+          onChange={handleRenderIntersectionChange}
+          rootMargin="2000px 0px" // A hardcoded reasonable value for lazy loading.
+          triggerOnce
+        />
+      )}
+      {!shouldReveal && (
+        <InView
+          className={classes.bounds}
+          onChange={handleRevealIntersectionChange}
+          rootMargin={rootMargin}
+          triggerOnce
+        />
       )}
 
-      {placeholder && (
-        <PlaceholderTransitionComponent
-          className={classnames(classes.bounds, placeholder.props.className)}
-          timeout={transitionDuration * 2}
-          in={!inProp}
-          unmountOnExit
-          {...PlaceholderTransitionProps}
-        >
-          {placeholder}
-        </PlaceholderTransitionComponent>
-      )}
+      {shouldRender && children}
+      {placeholder}
     </AspectRatio>
   )
 })
@@ -141,20 +192,17 @@ MediaLoader.propTypes = {
   children: elementAcceptingRef.isRequired,
   classes: PropTypes.object.isRequired,
   className: PropTypes.string,
-  component: PropTypes.elementType,
-  height: PropTypes.number,
+  in: PropTypes.bool,
   lazy: PropTypes.bool,
-  onLoad: PropTypes.func,
-  onRender: PropTypes.func,
-  onReveal: PropTypes.func,
+  onEnter: PropTypes.func,
+  onEntered: PropTypes.func,
+  onEntering: PropTypes.func,
+  onLoaded: PropTypes.func,
   placeholder: PropTypes.element,
-  PlaceholderTransitionComponent: PropTypes.elementType,
-  PlaceholderTransitionProps: PropTypes.object,
   rootMargin: PropTypes.string,
   TransitionComponent: PropTypes.elementType,
   transitionDuration: PropTypes.number,
   TransitionProps: PropTypes.object,
-  width: PropTypes.number,
 }
 
 MediaLoader.uiName = 'OuiMediaLoader'
