41affc2806bebf5e9bbd7fdc5bc0bc211a60b7d7
Martin Barri <maeertin@gmail.com>
2020-06-09 13:49:01 +0200

    [components] Add missing components to common exports file

--- packages/oui/src/index.js
+++ packages/oui/src/index.js
@@ -1,10 +1,19 @@
 /* eslint-disable import/export */
 
+export { default as AspectRatio } from './AspectRatio'
+export * from './AspectRatio'
+
+export { default as BackgroundMedia } from './BackgroundMedia'
+export * from './BackgroundMedia'
+
 export { default as Media } from './Media'
 export * from './Media'
 
 export { default as MediaLoader } from './MediaLoader'
 export * from './MediaLoader'
+
+export { default as Rwiper } from './Rwiper'
+export * from './Rwiper'
 
 export { default as ScrollProgress } from './ScrollProgress'
 export * from './ScrollProgress'
