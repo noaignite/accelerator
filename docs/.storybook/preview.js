0f9416d5fede1fa08b90ea6be4dd47b1cc8711d8
Martin Barri <maeertin@gmail.com>
2020-09-11 10:44:13 +0200

    [storybook] Migrate Storybook config to v6

---
+++ docs/.storybook/preview.js
@@ -0,0 +1,26 @@
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import CssBaseline from '@material-ui/core/CssBaseline'
+// As of NextJS 9, all global css *must* be imported in pages/_app.js
+// https://github.com/zeit/next.js/blob/master/errors/css-global.md
+import 'swiper/swiper-bundle.min.css'
+
+const theme = createMuiTheme({})
+
+export const parameters = {
+  layout: 'fullscreen',
+  options: {
+    storySort: {
+      method: 'alphabetical',
+    },
+  },
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+]
