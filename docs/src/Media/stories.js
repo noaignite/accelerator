133281c55c9d24958e119358f963544a19276611
Martin Barri <maeertin@gmail.com>
2020-09-11 11:58:56 +0200

    [docs] Migrate docs story templates to Storybook v6

--- docs/src/Media/stories.js
+++ docs/src/Media/stories.js
@@ -1,9 +1,5 @@
 import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import { boolean, text, select, object } from '@storybook/addon-knobs'
 import Media from '@oakwood/oui/Media'
-
-const stories = storiesOf('Components/Media', module)
 
 const sources = {
   jpg: {
@@ -24,79 +20,85 @@ const sources = {
   },
 }
 
-stories.add('Default', () => {
-  const comp = select('component', ['div', 'video', 'audio', 'picture', 'iframe', 'img'], 'div')
-
-  const componentProps = {}
-  if (['img', 'picture'].includes(comp)) {
-    componentProps.alt = text('alt', 'Just another image description')
-  }
-  if (['audio', 'video'].includes(comp)) {
-    componentProps.autoPlay = boolean('autoPlay', true)
-    componentProps.controls = boolean('controls', true)
-    componentProps.loop = boolean('loop', false)
-  }
-  if (['video'].includes(comp)) {
-    componentProps.muted = boolean('muted', false)
-    componentProps.playsInline = boolean('playsInline', false)
-  }
-
-  return (
-    <Media
-      component={comp}
-      src={select('src', [sources.jpg.xs, sources.mp3.xs, sources.mp4.xs], sources.jpg.xs)}
-      style={{ minHeight: '100px' }}
-      {...componentProps}
-    />
-  )
-})
-
-stories.add('Picture', () => (
-  <Media
-    component={select('component', ['picture'], 'picture')}
-    breakpoints={object('breakpoints', sources.jpg)}
-    src={text('src', sources.jpg.xs)}
-    alt={text('alt', 'Just another image description')}
-  />
-))
-
-stories.add('Picture w/ WebP', () => (
-  <Media
-    component={select('component', ['picture'], 'picture')}
-    breakpoints={object('breakpoints', {
-      xs: [{ src: sources.webp.xs, type: 'image/webp' }, { src: sources.jpg.xs }],
-      sm: [{ src: sources.webp.sm, type: 'image/webp' }, { src: sources.jpg.sm }],
-    })}
-    src={sources.jpg.xs}
-    alt={text('alt', 'Just another image description')}
-  />
-))
-
-stories.add('Video', () => (
-  <Media
-    component={select('component', ['video'], 'video')}
-    breakpoints={object('breakpoints', sources.mp4)}
-    autoPlay
-    controls
-    muted
-    loop
-  />
-))
-
-stories.add('Custom', () => (
-  <Media
-    breakpoints={object('breakpoints', {
-      xs: {
-        component: 'img',
-        src: sources.jpg.xs,
-        alt: 'Just another image description',
+export default {
+  title: 'Components/Media',
+  component: Media,
+  argTypes: {
+    component: {
+      control: {
+        type: 'select',
+        options: ['div', 'video', 'audio', 'picture', 'iframe', 'img'],
       },
-      sm: {
-        component: 'video',
-        src: sources.mp4.xs,
-        autoPlay: true,
-        muted: true,
+    },
+    src: {
+      control: {
+        type: 'select',
+        options: [sources.jpg.xs, sources.mp3.xs, sources.mp4.xs],
       },
-    })}
-  />
-))
+    },
+  },
+}
+
+const Template = (args) => <Media {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  component: 'div',
+  src: sources.jpg.xs,
+  alt: '',
+  style: { minHeight: 100 },
+}
+
+export const Picture = Template.bind({})
+Picture.args = {
+  component: 'picture',
+  breakpoints: sources.jpg,
+  alt: '',
+}
+
+export const PictureMultiFormat = Template.bind({})
+PictureMultiFormat.args = {
+  component: 'picture',
+  breakpoints: {
+    xs: [{ src: sources.webp.xs, type: 'image/webp' }, { src: sources.jpg.xs }],
+    sm: [{ src: sources.webp.sm, type: 'image/webp' }, { src: sources.jpg.sm }],
+  },
+  alt: '',
+}
+
+export const Audio = Template.bind({})
+Audio.args = {
+  component: 'audio',
+  src: sources.mp3.xs,
+  autoPlay: true,
+  controls: true,
+  loop: true,
+}
+
+export const Video = Template.bind({})
+Video.args = {
+  component: 'video',
+  src: sources.mp4.xs,
+  autoPlay: true,
+  controls: true,
+  loop: true,
+  muted: true,
+  playsInline: true,
+}
+
+export const Custom = Template.bind({})
+Custom.args = {
+  breakpoints: {
+    xs: {
+      component: 'img',
+      src: sources.jpg.xs,
+      alt: 'Just another image description',
+    },
+    sm: {
+      component: 'video',
+      src: sources.mp4.xs,
+      autoPlay: true,
+      muted: true,
+    },
+  },
+}
