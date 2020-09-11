5b1c16e5b3426604dfd6d3a8070994b806fffd66
Martin Barri <maeertin@gmail.com>
2020-09-11 11:58:17 +0200

    [storybook] Remove legacy config files

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,9 +1,7 @@
+import * as React from 'react'
 import ThemeProvider from '@material-ui/styles/ThemeProvider'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import CssBaseline from '@material-ui/core/CssBaseline'
-// As of NextJS 9, all global css *must* be imported in pages/_app.js
-// https://github.com/zeit/next.js/blob/master/errors/css-global.md
-import 'swiper/swiper-bundle.min.css'
 
 const theme = createMuiTheme({})
 
