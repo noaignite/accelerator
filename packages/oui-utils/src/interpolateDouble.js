14d9a130726f1eb41dadc855caf319388009ab9e
Dennis Holmen <dennis.holmen@gmail.com>
2020-10-01 23:20:43 +0200

    [utils] Updated interpolateDouble to pass-through new clamp parameter to mapRange

--- packages/oui-utils/src/interpolateDouble.js
+++ packages/oui-utils/src/interpolateDouble.js
@@ -1,14 +1,14 @@
 import mapRange from './mapRange'
 
-function interpolateDouble(value, inMin, inMid, inMax, outMin, outMid, outMax) {
+function interpolateDouble(value, inMin, inMid, inMax, outMin, outMid, outMax, clamp = false) {
   if (inMin < inMax) {
     return value > inMid
-      ? mapRange(value, inMid, inMax, outMid, outMax)
-      : mapRange(value, inMin, inMid, outMin, outMid)
+      ? mapRange(value, inMid, inMax, outMid, outMax, clamp)
+      : mapRange(value, inMin, inMid, outMin, outMid, clamp)
   }
   return value < inMid
-    ? mapRange(value, inMid, inMax, outMid, outMax)
-    : mapRange(value, inMin, inMid, outMin, outMid)
+    ? mapRange(value, inMid, inMax, outMid, outMax, clamp)
+    : mapRange(value, inMin, inMid, outMin, outMid, clamp)
 }
 
 export default interpolateDouble
