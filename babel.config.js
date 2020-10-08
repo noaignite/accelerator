df94de8dc9be281b0f643419c6a98974ce6955d7
Martin Barri <maeertin@gmail.com>
2020-10-08 12:18:06 +0200

    [core] Update webpack module resolver to use webpack-merge

--- babel.config.js
+++ babel.config.js
@@ -21,7 +21,6 @@ const defaultAlias = {
 
 const productionPlugins = [
   '@babel/plugin-transform-react-constant-elements',
-  'babel-plugin-transform-dev-warning',
   ['babel-plugin-react-remove-properties', { properties: ['data-testid'] }],
   [
     'babel-plugin-transform-react-remove-prop-types',
--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,17 +1,8 @@
-const deepmerge = require('@material-ui/utils/deepmerge').default
+const { merge } = require('webpack-merge')
 const webpackBaseConfig = require('./webpackBaseConfig')
 
 module.exports = {
   stories: ['../src/**/*stories.js'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  webpackFinal: async (config /*, { configType }*/) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    config = deepmerge(config, webpackBaseConfig)
-
-    // Return the altered config
-    return config
-  },
+  webpackFinal: async (config) => merge(config, webpackBaseConfig),
 }
