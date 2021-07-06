98a2e1021be37db1fed45d70610e7f0909ba9e9b
Martin Barri <maeertin@gmail.com>
2021-07-06 13:47:57 +0200

    [ScrollProgress] Pass dom target pass as an onChange argument

--- packages/oui/src/ScrollProgress/ScrollProgress.js
+++ packages/oui/src/ScrollProgress/ScrollProgress.js
@@ -24,7 +24,7 @@ const ScrollProgress = React.forwardRef(function ScrollProgress(props, ref) {
     const innerProgress = calculateVerticalProgress(bounds, maxOffset)
 
     if (onChange) {
-      onChange(progress, innerProgress, bounds)
+      onChange(progress, innerProgress, bounds, target)
     }
   }, [onChange])
 
