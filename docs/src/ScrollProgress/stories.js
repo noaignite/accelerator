f2cd87c32a29ecdda13726e99b435042f7b93fbf
Martin Barri <maeertin@gmail.com>
2021-08-30 17:18:32 +0200

    [docs] ScrollProgress api was changed to destructuring

--- docs/src/ScrollProgress/stories.js
+++ docs/src/ScrollProgress/stories.js
@@ -8,7 +8,7 @@ export default {
 
 const Template = (args) => {
   const [state, setState] = React.useState({})
-  const handleChange = React.useCallback((progress, innerProgress, bounds) => {
+  const handleChange = React.useCallback(({ progress, innerProgress, bounds }) => {
     setState({ progress, innerProgress, bounds })
   }, [])
 
