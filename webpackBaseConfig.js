6b89b54ee968ab2b2739dc001c86d2177eff934c
Martin Barri <maeertin@gmail.com>
2021-11-11 18:08:50 +0100

    build: add babel/webpack aliases for new utils package

--- webpackBaseConfig.js
+++ webpackBaseConfig.js
@@ -7,7 +7,7 @@ module.exports = {
     alias: {
       '@noaignite/formit': path.join(__dirname, './packages/formit/src'),
       '@noaignite/oui': path.join(__dirname, './packages/oui/src'),
-      '@noaignite/oui-utils': path.join(__dirname, './packages/oui-utils/src'),
+      '@noaignite/utils': path.join(__dirname, './packages/utils/src'),
     },
   },
 }
