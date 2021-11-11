913ac69931477659b9a26145fe736b746679e314
Martin Barri <maeertin@gmail.com>
2021-11-11 17:33:43 +0100

    feat(oui): add new getObserverInstance util to manage intersection observer instances

--- packages/oui/src/utils/index.js
+++ packages/oui/src/utils/index.js
@@ -1,3 +1,4 @@
+export { default as getObserverInstance } from './getObserverInstance'
 export { default as mediaBreakpointsType } from './mediaBreakpointsType'
 export { default as mediaBreakpointType } from './mediaBreakpointType'
 export { default as pictureSourceType } from './pictureSourceType'
