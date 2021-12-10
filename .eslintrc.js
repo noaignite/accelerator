a65c4ec7e961ba9cb0c4198b6d88ed07f0d493db
Martin Barri <maeertin@gmail.com>
2021-12-10 23:09:57 +0100

    chore: init @noaignite/eslint-config
    Init new shareable eslint config package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  root: true, // So parent files don't get applied
-  globals: {
-    preval: false,
-  },
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
-  extends: ['plugin:import/recommended', 'airbnb', 'prettier'],
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 7,
-  },
-  plugins: ['eslint-plugin-react-hooks'],
+  extends: ['@noaignite/eslint-config'],
   settings: {
     'import/resolver': {
       webpack: {
@@ -23,95 +9,9 @@ module.exports = {
       },
     },
   },
-  /**
-   * Sorted alphanumerically within each group. built-in and each plugin form
-   * their own groups.
-   */
-  rules: {
-    // Stylistic opinion
-    'arrow-body-style': 'off',
-    // Consistency is key
-    'consistent-this': ['error', 'self'],
-    curly: ['error', 'all'],
-    // just as bad as "max components per file"
-    'max-classes-per-file': 'off',
-    // Too interruptive
-    'no-alert': 'error',
-    // Strict, airbnb is using warn; allow warn and error for dev environments
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-param-reassign': 'off', // It's fine.
-    'no-constant-condition': 'error',
-    // Use the proptype inheritance chain
-    'no-prototype-builtins': 'off',
-    // Allow default exports
-    'no-restricted-exports': 'off',
-    'no-underscore-dangle': 'error',
-    'nonblock-statement-body-position': 'error',
-    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
-    // Destructuring harm grep potential.
-    'prefer-destructuring': 'off',
-
-    // Not sure why it doesn't work
-    // 'import/named': 'off',
-    // Missing yarn workspace support
-    'import/no-extraneous-dependencies': 'off',
-    // The code is already coupled to webpack. Prefer explicit coupling.
-    'import/no-webpack-loader-syntax': 'off',
-
-    'jsx-a11y/label-has-associated-control': 'off',
-    'jsx-a11y/no-autofocus': 'off',
-
-    'react-hooks/exhaustive-deps': ['error', { additionalHooks: 'useEnhancedEffect' }],
-    'react-hooks/rules-of-hooks': 'error',
-
-    'react/default-props-match-prop-types': [
-      'error',
-      {
-        // Otherwise the rule thinks inner props = outer props
-        // But in TypeScript we want to know that a certain prop is defined during render
-        // while it can be ommitted from the callsite.
-        // Then defaultProps (or default values) will make sure that the prop is defined during render
-        allowRequiredDefaults: true,
-      },
-    ],
-    // Can add verbosity to small functions making them harder to grok.
-    // Though we have to manually enforce it for function components with default values.
-    'react/destructuring-assignment': 'off',
-    'react/forbid-prop-types': 'off', // Too strict, no time for that
-    'react/function-component-definition': 'off', // Could this be useful?
-    'react/jsx-curly-brace-presence': 'off', // broken
-    // airbnb is using .jsx
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.tsx'] }],
-    // Prefer <React.Fragment> over <>.
-    'react/jsx-fragments': ['error', 'element'],
-    // Enforces premature optimization
-    'react/jsx-no-bind': 'off',
-    // We are a UI library.
-    'react/jsx-props-no-spreading': 'off',
-    // This rule is great for raising people awareness of what a key is and how it works.
-    'react/no-array-index-key': 'off',
-    'react/no-danger': 'error',
-    'react/no-direct-mutation-state': 'error',
-    // Not always relevant
-    'react/require-default-props': 'off',
-    // This depends entirely on what you're doing. There's no universal pattern
-    'react/state-in-constructor': 'off',
-    // stylistic opinion. For conditional assignment we want it outside, otherwise as static
-    'react/static-property-placement': 'off',
-  },
   overrides: [
     {
-      files: ['test/**/*', '*.test.js', '*.test.ts', '*.test.tsx'],
-      env: {
-        jest: true,
-      },
-      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
-      rules: {
-        'testing-library/render-result-naming-convention': 'off',
-      },
-    },
-    {
-      files: ['docs/src/**/*.js'],
+      files: ['docs/**/*'],
       rules: {
         'react/prop-types': 'off',
       },
---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,147 @@
+// Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:import/recommended', 'airbnb', 'prettier'],
+  parser: '@babel/eslint-parser',
+  plugins: ['eslint-plugin-react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      extends: [
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'airbnb-typescript',
+        'prettier',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      // If adding a typescript-eslint version of an existing ESLint rule,
+      // make sure to disable the ESLint rule here.
+      rules: {
+        // disabled type-aware linting due to performance considerations
+        '@typescript-eslint/dot-notation': 'off',
+        'dot-notation': 'error',
+        // disabled type-aware linting due to performance considerations
+        '@typescript-eslint/no-implied-eval': 'off',
+        'no-implied-eval': 'error',
+        // disabled type-aware linting due to performance considerations
+        '@typescript-eslint/no-throw-literal': 'off',
+        'no-throw-literal': 'error',
+        // disabled type-aware linting due to performance considerations
+        '@typescript-eslint/return-await': 'off',
+        'no-return-await': 'error',
+      },
+    },
+    {
+      files: ['test/**/*', '*.test.js', '*.test.ts', '*.test.tsx'],
+      env: {
+        jest: true,
+      },
+      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
+      rules: {
+        'testing-library/render-result-naming-convention': 'off',
+      },
+    },
+  ],
+  /**
+   * Sorted alphanumerically within each group. built-in and each plugin form
+   * their own groups.
+   *
+   * NOTE: When adding rules here, you need to make sure they are compatible with
+   * `typescript-eslint`, as some rules such as `no-array-constructor` aren't compatible.
+   */
+  rules: {
+    // Stylistic opinion
+    'arrow-body-style': 'off',
+    // Consistency is key
+    'consistent-this': ['error', 'self'],
+    curly: ['error', 'all'],
+    // just as bad as "max components per file"
+    'max-classes-per-file': 'off',
+    // Too interruptive
+    'no-alert': 'error',
+    // Strict, airbnb is using warn; allow warn and error for dev environments
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-param-reassign': 'off', // It's fine.
+    'no-constant-condition': 'error',
+    // Use the proptype inheritance chain
+    'no-prototype-builtins': 'off',
+    // Allow default exports
+    'no-restricted-exports': 'off',
+    'no-underscore-dangle': 'error',
+    'nonblock-statement-body-position': 'error',
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    // Destructuring harm grep potential.
+    'prefer-destructuring': 'off',
+
+    // Not sure why it doesn't work
+    // 'import/named': 'off',
+    // Missing yarn workspace support
+    'import/no-extraneous-dependencies': 'off',
+    // The code is already coupled to webpack. Prefer explicit coupling.
+    'import/no-webpack-loader-syntax': 'off',
+
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-autofocus': 'off',
+
+    'react-hooks/exhaustive-deps': ['error', { additionalHooks: 'useEnhancedEffect' }],
+    'react-hooks/rules-of-hooks': 'error',
+
+    'react/default-props-match-prop-types': [
+      'error',
+      {
+        // Otherwise the rule thinks inner props = outer props
+        // But in TypeScript we want to know that a certain prop is defined during render
+        // while it can be ommitted from the callsite.
+        // Then defaultProps (or default values) will make sure that the prop is defined during render
+        allowRequiredDefaults: true,
+      },
+    ],
+    // Can add verbosity to small functions making them harder to grok.
+    // Though we have to manually enforce it for function components with default values.
+    'react/destructuring-assignment': 'off',
+    'react/forbid-prop-types': 'off', // Too strict, no time for that
+    'react/function-component-definition': 'off', // Could this be useful?
+    'react/jsx-curly-brace-presence': 'off', // broken
+    // airbnb is using .jsx
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.tsx'] }],
+    // Prefer <React.Fragment> over <>.
+    'react/jsx-fragments': ['error', 'element'],
+    // Enforces premature optimization
+    'react/jsx-no-bind': 'off',
+    // We are a UI library.
+    'react/jsx-props-no-spreading': 'off',
+    // This rule is great for raising people awareness of what a key is and how it works.
+    'react/no-array-index-key': 'off',
+    'react/no-danger': 'error',
+    'react/no-direct-mutation-state': 'error',
+    // Not always relevant
+    'react/require-default-props': 'off',
+    // This depends entirely on what you're doing. There's no universal pattern
+    'react/state-in-constructor': 'off',
+    // stylistic opinion. For conditional assignment we want it outside, otherwise as static
+    'react/static-property-placement': 'off',
+  },
+}
