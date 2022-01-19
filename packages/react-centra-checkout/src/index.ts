5b41fbf2c600d935d6c385a03a5efe2358783c5d
alexanderflink <hi@alexanderflink.com>
2022-01-19 18:01:37 +0100

    style: rearrange exports

--- packages/react-centra-checkout/src/index.ts
+++ packages/react-centra-checkout/src/index.ts
@@ -1,9 +1,8 @@
 /* eslint-disable import/export */
-
 export { default as ApiClient } from './ApiClient'
+export * from './ApiClient'
 
 export { default as PaymentForm } from './PaymentForm'
-export * from './ApiClient'
 
 export { default } from './Context'
 export * from './Context'
