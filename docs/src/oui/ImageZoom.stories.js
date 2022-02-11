d51ebdb0a4889758482efeead161ed4800ec0dbe
Martin Barri <maeertin@gmail.com>
2022-02-11 14:08:56 +0100

    docs(ImageZoom): small storybook template tweak

--- docs/src/oui/ImageZoom.stories.js
+++ docs/src/oui/ImageZoom.stories.js
@@ -7,30 +7,34 @@ export default {
 }
 
 const Template1 = (args) => (
-  <ImageZoom style={{ margin: 100 }} {...args}>
-    <img
-      src="//source.unsplash.com/DmD8HVOjy4c/400x500"
-      alt=""
-      style={{ display: 'block', width: '100%' }}
-    />
+  <div style={{ minHeight: '150vh', padding: '10vh 10vw' }}>
+    <ImageZoom {...args}>
+      <img
+        src="//source.unsplash.com/DmD8HVOjy4c/400x500"
+        alt=""
+        style={{ display: 'block', width: '100%' }}
+      />
 
-    <img
-      src="//source.unsplash.com/DmD8HVOjy4c/1200x1500"
-      alt=""
-      style={{ display: 'block', width: '100%' }}
-    />
-  </ImageZoom>
+      <img
+        src="//source.unsplash.com/DmD8HVOjy4c/1200x1500"
+        alt=""
+        style={{ display: 'block', width: '100%' }}
+      />
+    </ImageZoom>
+  </div>
 )
 
 export const Default = Template1.bind({})
 Default.args = {}
 
 const Template2 = (args) => (
-  <ImageZoom style={{ margin: 100 }} {...args}>
-    <Media src="//source.unsplash.com/DmD8HVOjy4c/400x500" />
+  <div style={{ minHeight: '150vh', padding: '10vh 10vw' }}>
+    <ImageZoom {...args}>
+      <Media src="//source.unsplash.com/DmD8HVOjy4c/400x500" />
 
-    <Media src="//source.unsplash.com/DmD8HVOjy4c/1200x1500" />
-  </ImageZoom>
+      <Media src="//source.unsplash.com/DmD8HVOjy4c/1200x1500" />
+    </ImageZoom>
+  </div>
 )
 
 export const WithMedia = Template2.bind({})
