68be150e1c010835c7509d1216affa4edb78b4c9
Martin Barri <maeertin@gmail.com>
2022-08-03 17:47:58 +0200

    fix(createMediaPreload): now correctly renders single link tag when component is img

--- packages/oui/src/utils/createMediaPreload.js
+++ packages/oui/src/utils/createMediaPreload.js
@@ -1,23 +1,23 @@
 import * as React from 'react'
 
-const SINGLE_SRC_MEDIA_COMPONENTS = ['img', 'video', 'audio', 'iframe']
+const SINGLE_SRC_MEDIA_TYPES = ['image', 'video', 'audio', 'iframe']
 
-export default function createMediaPreload(HeadComponent) {
-  return function mediaPreload(props) {
-    const { component, sources, src } = props
+export default function createMediaPreload(Wrapper = React.Fragment) {
+  return function mediaPreload(options = {}) {
+    const { component, sources, src } = options
 
     const mediaType = component === 'img' ? 'image' : component
 
     return (
-      <HeadComponent>
+      <Wrapper>
         {sources?.map((source) => (
           <link key={source.src} rel="preload" as="image" href={source.src} media={source.media} />
         ))}
 
-        {src && SINGLE_SRC_MEDIA_COMPONENTS.includes(mediaType) && (
+        {src && SINGLE_SRC_MEDIA_TYPES.includes(mediaType) && (
           <link key={src} rel="preload" as={mediaType} href={src} />
         )}
-      </HeadComponent>
+      </Wrapper>
     )
   }
 }
