21c9d16c48d14538c22f8a5358b782d9d08772ae
Martin Barri <maeertin@gmail.com>
2023-02-12 11:49:25 +0100

    build(copy-files.js): update how main, module & types entries are injected

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -23,11 +23,48 @@ async function includeFileInBuild(file) {
  * @param {string} rootDir
  */
 async function createModulePackages({ from, to }) {
-  const directoryPackages = glob.sync('*/index.js', { cwd: from }).map(path.dirname)
+  const directoryPackages = glob.sync('*/index.{js,ts,tsx}', { cwd: from }).map(path.dirname)
 
   await Promise.all(
     directoryPackages.map(async (directoryPackage) => {
       const packageJsonPath = path.join(to, directoryPackage, 'package.json')
+      const topLevelPathImportsAreCommonJSModules = await fse.pathExists(
+        path.resolve(path.dirname(packageJsonPath), '../esm'),
+      )
+
+      const packageJson = {
+        sideEffects: false,
+        module: topLevelPathImportsAreCommonJSModules
+          ? path.posix.join('../esm', directoryPackage, 'index.js')
+          : './index.js',
+        main: topLevelPathImportsAreCommonJSModules
+          ? './index.js'
+          : path.posix.join('../node', directoryPackage, 'index.js'),
+        types: './index.d.ts',
+      }
+
+      const [typingsEntryExist, moduleEntryExists, mainEntryExists] = await Promise.all([
+        fse.pathExists(path.resolve(path.dirname(packageJsonPath), packageJson.types)),
+        fse.pathExists(path.resolve(path.dirname(packageJsonPath), packageJson.module)),
+        fse.pathExists(path.resolve(path.dirname(packageJsonPath), packageJson.main)),
+        fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2)),
+      ])
+
+      const manifestErrorMessages = []
+      if (!typingsEntryExist) {
+        // TODO: re-add check once all has been migrated to TypeScript.
+        // manifestErrorMessages.push(`'types' entry '${packageJson.types}' does not exist`)
+      }
+      if (!moduleEntryExists) {
+        manifestErrorMessages.push(`'module' entry '${packageJson.module}' does not exist`)
+      }
+      if (!mainEntryExists) {
+        manifestErrorMessages.push(`'main' entry '${packageJson.main}' does not exist`)
+      }
+      if (manifestErrorMessages.length > 0) {
+        // TODO: AggregateError
+        throw new Error(`${packageJsonPath}:\n${manifestErrorMessages.join('\n')}`)
+      }
 
       return packageJsonPath
     }),
@@ -37,16 +74,27 @@ async function createModulePackages({ from, to }) {
 async function createPackageFile() {
   const packageData = await fse.readFile(path.resolve(packagePath, './package.json'), 'utf8')
   const { scripts, devDependencies, workspaces, ...packageDataOther } = JSON.parse(packageData)
+
   const newPackageData = {
     ...packageDataOther,
     private: false,
-    ...(packageDataOther.main && {
-      main: './node/index.js',
-    }),
-    ...(packageDataOther.module && {
-      module: './index.js',
-    }),
+    ...(packageDataOther.main
+      ? {
+          main: fse.existsSync(path.resolve(buildPath, './node/index.js'))
+            ? './node/index.js'
+            : './index.js',
+          module: fse.existsSync(path.resolve(buildPath, './esm/index.js'))
+            ? './esm/index.js'
+            : './index.js',
+        }
+      : {}),
   }
+
+  const typeDefinitionsFilePath = path.resolve(buildPath, './index.d.ts')
+  if (await fse.pathExists(typeDefinitionsFilePath)) {
+    newPackageData.types = './index.d.ts'
+  }
+
   const targetPath = path.resolve(buildPath, './package.json')
 
   await fse.writeFile(targetPath, JSON.stringify(newPackageData, null, 2), 'utf8')
