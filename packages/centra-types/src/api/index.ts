0e611eca0bd650d7625188f5dd3a6d5027fa6511
Alexander Flink <hi@alexanderflink.com>
2023-02-27 14:53:46 +0100

    fix(exports): add types keyword

--- packages/centra-types/src/api/index.ts
+++ packages/centra-types/src/api/index.ts
@@ -1,6 +1,6 @@
-export { default as BundleResponse } from './Bundle'
+export type { default as BundleResponse } from './Bundle'
 
-export { default as PricelistsResponse } from './Pricelists'
+export type { default as PricelistsResponse } from './Pricelists'
 
-export { default as ProductResponse } from './Product'
+export type { default as ProductResponse } from './Product'
 export * from './Product'
--- packages/centra-types/src/models/index.ts
+++ packages/centra-types/src/models/index.ts
@@ -1,53 +1,53 @@
-export { default as Address } from './Address'
+export type { default as Address } from './Address'
 export * from './Address'
 
-export { default as BrickAndMortar } from './BrickAndMortar'
+export type { default as BrickAndMortar } from './BrickAndMortar'
 export * from './BrickAndMortar'
 
-export { default as Bundle } from './Bundle'
+export type { default as Bundle } from './Bundle'
 
-export { default as BundleInfo } from './BundleInfo'
+export type { default as BundleInfo } from './BundleInfo'
 export * from './BundleInfo'
 
-export { default as Category } from './Category'
+export type { default as Category } from './Category'
 
-export { default as Country } from './Country'
+export type { default as Country } from './Country'
 export * from './Country'
 
-export { default as Currency } from './Currency'
+export type { default as Currency } from './Currency'
 
-export { default as Discounts } from './Discounts'
+export type { default as Discounts } from './Discounts'
 
-export { default as Filter } from './Filter'
+export type { default as Filter } from './Filter'
 
-export { default as ImageAttribute } from './ImageAttribute'
+export type { default as ImageAttribute } from './ImageAttribute'
 
-export { default as Item } from './Item'
+export type { default as Item } from './Item'
 
-export { default as Language } from './Language'
+export type { default as Language } from './Language'
 
-export { default as Location } from './Location'
+export type { default as Location } from './Location'
 
-export { default as LoggedIn } from './LoggedIn'
+export type { default as LoggedIn } from './LoggedIn'
 
-export { default as Order } from './Order'
+export type { default as Order } from './Order'
 export * from './Order'
 
-export { default as Payment } from './Payment'
+export type { default as Payment } from './Payment'
 
-export { default as PaymentMethod } from './PaymentMethod'
+export type { default as PaymentMethod } from './PaymentMethod'
 
-export { default as PluginFields } from './PluginFields'
+export type { default as PluginFields } from './PluginFields'
 
-export { default as Pricelist } from './Pricelist'
+export type { default as Pricelist } from './Pricelist'
 
-export { default as Product } from './Product'
+export type { default as Product } from './Product'
 export * from './Product'
 
-export { default as Response } from './Response'
+export type { default as Response } from './Response'
 export * from './Response'
 
-export { default as Selection } from './Selection'
+export type { default as Selection } from './Selection'
 export * from './Selection'
 
-export { default as Shipment } from './Shipment'
+export type { default as Shipment } from './Shipment'
