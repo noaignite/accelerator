dc55792620aa061c315fc555a02df949aa584c61
Alexander Flink <hi@alexanderflink.com>
2023-03-02 11:32:04 +0100

    fix(ApiClient): make sure headers are not overwritten

--- packages/react-centra-checkout/src/ApiClient/index.ts
+++ packages/react-centra-checkout/src/ApiClient/index.ts
@@ -1,27 +1,29 @@
 class ApiClient {
   baseUrl: string
 
+  headers: Headers
+
   options: RequestInit
 
   static default: ApiClient
 
-  headers: Headers = new Headers({
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  })
-
   constructor(baseUrl = '', options: RequestInit = {}) {
     this.baseUrl = baseUrl
     this.options = options
+    this.headers = new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...this.options.headers,
+    })
   }
 
   request = async (method: string, endpoint: string, data: Record<string, unknown> = {}) => {
     const response = await fetch(`${this.baseUrl}/${endpoint}`, {
+      ...this.options,
       headers: this.headers,
       mode: 'cors',
       method,
       body: ['POST', 'PUT'].includes(method) ? JSON.stringify(data) : undefined,
-      ...this.options,
     })
 
     const json = await response.json()
