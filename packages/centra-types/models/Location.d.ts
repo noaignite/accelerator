c21892b740e5502124aaef6272db1fa553ec60df
Alexander Flink <hi@alexanderflink.com>
2023-03-03 15:07:01 +0100

    fix(Location): add missing pricelist

--- packages/centra-types/models/Location.d.ts
+++ packages/centra-types/models/Location.d.ts
@@ -3,9 +3,10 @@ import Language from './Language'
 export default interface Location {
   country?: string
   name?: string
-  state?: string
+  state?: string | null
   stateName?: string
   eu?: boolean
   shipTo?: boolean
   language?: Language
+  pricelist?: number
 }
