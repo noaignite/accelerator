cf728136f301a9ef7848eeee8a6a3f705b675a44
Alexander Flink <hi@alexanderflink.com>
2023-03-09 15:36:58 +0100

    feat: add Market, update Address and Location

--- packages/centra-types/models/Location.d.ts
+++ packages/centra-types/models/Location.d.ts
@@ -2,11 +2,12 @@ import Language from './Language'
 
 export default interface Location {
   country?: string
+  eu?: boolean
+  language?: Language
+  market?: string
   name?: string
+  pricelist?: number
+  shipTo?: boolean
   state?: string | null
   stateName?: string
-  eu?: boolean
-  shipTo?: boolean
-  language?: Language
-  pricelist?: number
 }
