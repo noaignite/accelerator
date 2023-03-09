66972a4daed036b710b6170d5094d4cb6624e4db
Alexander Flink <hi@alexanderflink.com>
2023-03-09 14:05:17 +0100

    fix(Filter): add missing FilterValue type

--- packages/centra-types/models/Filter.d.ts
+++ packages/centra-types/models/Filter.d.ts
@@ -1,5 +1,12 @@
 export default interface Filter {
   field?: string
-  // TODO?: add type for filter value
-  values?: unknown[]
+  values?: FilterValue[]
+}
+
+export interface FilterValue {
+  value?: string
+  count?: number
+  filterCount?: number
+  totalCount?: number
+  data?: string | Record<string, string | string[]>
 }
