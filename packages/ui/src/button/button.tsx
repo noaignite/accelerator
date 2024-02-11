e405d8169a30eaff2339efb4894986da7c4b3887
adamsoderstrom <adamsoderstrom@me.com>
2024-02-11 16:59:40 +0100

    add StyleX to `ui` package

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,6 +1,29 @@
 import { ReactNode } from "react";
-import clsx from "clsx";
-import classes from "./button.module.css";
+import * as stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  base: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    borderStyle: 'none',
+    boxSizing: 'border-box',
+    color: '#000000',
+    cursor: 'pointer',
+    display: 'inline-block',
+    fontSize: 16,
+    height: 40,
+    lineHeight: '20px',
+    listStyle: 'none',
+    margin: 0,
+    outline: 'none',
+    padding: '10px 16px',
+    position: 'relative',
+    textAlign: 'center',
+    textDecoration: 'none',
+    transition: 'color 100ms',
+    verticalAlign: 'baseline',
+  }
+})
 
 interface ButtonProps {
   children: ReactNode;
@@ -9,11 +32,11 @@ interface ButtonProps {
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, className, appName } = props;
+  const { children, appName } = props;
 
   return (
     <button
-      className={clsx(classes.root, className)}
+      {...stylex.props(styles.base)}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
