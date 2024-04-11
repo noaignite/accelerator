9754c702d581ea0016c99c40a4ea1033f0ed27c9
adamsoderstrom <adamsoderstrom@me.com>
2024-04-11 15:23:19 +0200

    chore: migrate `@noaignite/centra-types` to new setup

---
+++ packages/centra-types/models/PaymentField.d.ts
@@ -0,0 +1,5 @@
+export default interface PaymentField {
+  type?: string
+  visible?: boolean
+  required?: boolean
+}
---
+++ packages/centra-types/models/PaymentMethod.d.ts
@@ -0,0 +1,8 @@
+export default interface PaymentMethod {
+  paymentMethod?: string
+  name?: string
+  handlingCost?: string
+  handlingCostAsNumber?: number
+  supportsInitiateOnly?: boolean
+  providesCustomerAddressAfterPayment?: boolean
+}
