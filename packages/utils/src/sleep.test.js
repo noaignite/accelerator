7691fa73f3bf60fceba7dcec4ee67adacad631ca
adamsoderstrom <adamsoderstrom@me.com>
2024-04-11 14:55:17 +0200

    chore: add `@adamsoderstrom/ui` package

---
+++ packages/utils/src/sleep.test.js
@@ -0,0 +1,31 @@
+import sleep from './sleep'
+
+describe('sleep', () => {
+  it('async await', async () => {
+    const start = performance.now()
+    await sleep(20)
+    expect(performance.now() - start).toBeGreaterThanOrEqual(19)
+  })
+
+  it('then', async () => {
+    const start = performance.now()
+    return sleep(20).then(() => {
+      expect(performance.now() - start).toBeGreaterThanOrEqual(19)
+    })
+  })
+
+  it('delayed sleep', async () => {
+    const start = performance.now()
+
+    const sleepPromise = sleep(20)
+
+    await sleep(20)
+
+    return sleepPromise.then(() => {
+      const end = performance.now()
+
+      expect(end - start).toBeGreaterThanOrEqual(19)
+      expect(end - start).toBeLessThan(30)
+    })
+  })
+})
