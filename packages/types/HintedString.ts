12fdf68615f9540994005572a155f45eb30eaaf8
Dash <donnergren.work@gmail.com>
2024-04-17 11:14:22 +0200

    add additional type utilities to type package

---
+++ packages/types/HintedString.ts
@@ -0,0 +1,12 @@
+/**
+ * Prevent TypeScript from collapsing a union of string literals into `string`.
+ * Useful when union should be part of IntelliSense suggestions, but any `string`
+ * value is also accepted.
+ *
+ * @example
+ * type Union = 'foo' | 'bar'
+ *
+ * type Unhinted = Union | string // string
+ * type Hinted = HintedString<Union> // 'foo' | 'bar' | string
+ */
+export type HintedString<Union extends string> = Union | (string & {})
