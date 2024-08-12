2a5e793c30601185dbb62a23cb322b12130ebf5f
adamsoderstrom <adamsoderstrom@me.com>
2024-08-12 10:52:49 +0200

    fix(react-centra-checkout): return void from `init` onMount of `Context`

--- packages/react-centra-checkout/src/Context/index.tsx
+++ packages/react-centra-checkout/src/Context/index.tsx
@@ -527,8 +527,7 @@ export function CentraProvider(props: ProviderProps) {
 
   useEffect(() => {
     if (!disableInit) {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises -- TODO: FIx this
-      init()
+      void init()
     }
 
     // always add event listener for centra_checkout_callback in case it is used
