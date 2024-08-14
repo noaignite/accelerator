edf3db3eaf011f35c75e5716555a9a271b4c6f08
Martin Barri <maeertin@gmail.com>
2024-08-14 14:05:53 +0200

    build(react-utils): drop commonJS support

--- packages/react-utils/tsup.config.ts
+++ packages/react-utils/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/**/*.{ts,tsx}'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   bundle: false, // NOTE: Disable `bundle` as this causes issues with the 'use client' directive.
   clean: true,
   dts: true,
