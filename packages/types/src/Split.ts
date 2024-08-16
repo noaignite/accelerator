2efe4459d20e3f50aae30af66b6a76f983403ad5
Martin Barri <maeertin@gmail.com>
2024-08-16 10:24:00 +0200

    feat(types): add new Split type helper

---
+++ packages/types/src/Split.ts
@@ -0,0 +1,18 @@
+/**
+ * Split a string into an array of substrings using the specified separator
+ * and return the result.
+ *
+ * @example
+ * ```ts
+ * type T0 = Split<'a-b-c', '-'>; // ['a', 'b', 'c']
+ * type T1 = Split<'a-b-c', '.'>; // ['a-b-c']
+ * type T2 = Split<'a-b-c', ''>; // ['a', '-', 'b', '-', 'c']
+ * ```
+ */
+export type Split<S extends string, D extends string> = string extends S
+  ? string[]
+  : S extends ''
+  ? []
+  : S extends `${infer T}${D}${infer U}`
+  ? [T, ...Split<U, D>]
+  : [S]
