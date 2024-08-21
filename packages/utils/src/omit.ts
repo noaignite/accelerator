a00cb747b54aeed5336562801eb7d7f8dc36ddba
Martin Barri <maeertin@gmail.com>
2024-08-21 18:18:22 +0200

    feat(utils): add new omit helper

---
+++ packages/utils/src/omit.ts
@@ -0,0 +1,23 @@
+import { type KeyOfAny } from '@noaignite/types'
+
+/**
+ * Omit keys from object.
+ *
+ * @param object - Object to omit keys from.
+ * @param keys - Array of keys to omit from object.
+ *
+ * @example
+ * ```ts
+ * omit({ a: 1, b: 2, c: 3 }, ['a', 'c']) // { b: 2 }
+ * ```
+ */
+export default function omit<T extends Record<KeyOfAny, unknown>, K extends keyof T>(
+  obj: T,
+  keys: K[],
+) {
+  const entries = Object.entries(obj) as [K, unknown][]
+  const filtered = entries.filter(([key]) => !keys.includes(key))
+  const result = Object.fromEntries(filtered) as Omit<T, K>
+
+  return result
+}
