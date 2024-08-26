4fcbf6076edb4dc856ed0a19e1ccf62783e5beba
Martin Barri <maeertin@gmail.com>
2024-08-26 13:39:50 +0200

    chore(react-utils): disable  eslint rule

--- packages/react-utils/tsup.config.ts
+++ packages/react-utils/tsup.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   esbuildPlugins: [
     preserveDirectivesPlugin({
       directives: ['use client', 'use strict'],
+      // eslint-disable-next-line prefer-named-capture-group -- Is this a necessary rule?
       include: /\.(js|ts|jsx|tsx)$/,
       exclude: /node_modules/,
     }),
