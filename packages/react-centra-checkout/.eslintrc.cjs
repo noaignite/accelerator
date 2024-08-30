90c25825f84cebe937e888120b25279ee0e18703
adamsoderstrom <adamsoderstrom@me.com>
2024-08-30 16:50:18 +0200

    build(react-centra-checkout): `checksVoidReturn: false`

--- packages/react-centra-checkout/.eslintrc.cjs
+++ packages/react-centra-checkout/.eslintrc.cjs
@@ -1,4 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['@repo/eslint-config/react.js'],
+  rules: {
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": false
+      }
+    ]
+  }
 }
