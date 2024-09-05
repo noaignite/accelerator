265dfb4dbd1fd4c62baa3481475157a4c4cf68c5
Martin Barri <maeertin@gmail.com>
2024-09-05 15:52:08 +0200

    feat(react-hooks): add preserveDirectivesPlugin plugin for correct bundling of directives

--- packages/react-hooks/tsup.config.ts
+++ packages/react-hooks/tsup.config.ts
@@ -1,11 +1,20 @@
+// @ts-expect-error - TODO: Create PR & remove `type: "module"` at https://github.com/Seojunhwan/esbuild-plugin-preserve-directives
+import { preserveDirectivesPlugin } from 'esbuild-plugin-preserve-directives'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/**/*.{ts,tsx}'],
   format: ['esm'],
-  bundle: false, // NOTE: Disable `bundle` as this causes issues with the 'use client' directive.
   clean: true,
   dts: true,
   minify: true,
   sourcemap: true,
+  esbuildPlugins: [
+    preserveDirectivesPlugin({
+      directives: ['use client', 'use strict'],
+      // eslint-disable-next-line prefer-named-capture-group -- Is this a necessary rule?
+      include: /\.(js|ts|jsx|tsx)$/,
+      exclude: /node_modules/,
+    }),
+  ],
 })
