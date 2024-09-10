1d6cc48893914b0050f6be9e633b0a8607717841
adamsoderstrom <adamsoderstrom@me.com>
2024-09-10 14:58:31 +0200

    build(centra-mocks): include `*.d.ts` files in output
    Without `*.d.ts` files, we can't properly infer the types of the mocks
    in `*.ts` files. If we wan't consume the mocks in `*.ts` files without
    "implicit any" warnings, we have to include `*.d.ts` files.

--- packages/centra-mocks/tsup.config.ts
+++ packages/centra-mocks/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   format: ['esm', 'cjs'],
   bundle: false, // TODO: Do we even need to bundle a mock package?
   clean: true,
-  dts: false, // TODO: Do we even need dts for a mock package?
+  dts: true,
   minify: false, // TODO: Do we even need minify a mock package?
   sourcemap: false, // TODO: Do we even need sourcemaps for a mock package?
 })
