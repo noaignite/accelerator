1ddcccbba44ad9a00ded249a0d9999aedbc54529
Dash <i@dashice.com>
2024-09-16 15:01:14 +0200

    feat(react-utils): add useIntersectionObserver hook

---
+++ packages/react-utils/src/useIntersectionObserver.ts
@@ -0,0 +1,79 @@
+'use client'
+
+import type { RefObject } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useIsomorphicEffect } from './useIsomorphicEffect'
+
+export type UseIntersectionObserverCallback = (entry: IntersectionObserverEntry) => void
+
+export type IntersectionObserverOptions = IntersectionObserverInit & {
+  /**
+   * A `boolean` indicating whether the hook is enabled.
+   *
+   * @defaultValue true
+   */
+  when?: boolean
+  /**
+   * A `boolean` indicating whether the first intersection should terminate the observer.
+   *
+   * @defaultValue false
+   */
+  once?: boolean
+}
+
+/**
+ * Registers an `IntersectionObserver` on `ref` and executes `callback` when
+ * the observed element intersects with the `root` element.
+ *
+ * @param ref - A `RefObject` of the element to observe.
+ * @param callback - A function to execute when the observed element intersection changes.
+ * @param options - Configurable options
+ *
+ * @returns void
+ *
+ * @example
+ * ```tsx
+ * const ref = useRef<HTMLSpanElement>(null);
+ * const [shouldLoadImage, setShouldLoadImage] = useState(false);
+ *
+ * useIntersectionObserver(ref, (entry) => {
+ *   setShouldLoadImage(entry.isIntersecting);
+ * }, { once: true })
+ * ```
+ */
+export const useIntersectionObserver = (
+  ref: RefObject<Element | null>,
+  callback: UseIntersectionObserverCallback,
+  { when = true, once = false, root, rootMargin, threshold }: IntersectionObserverOptions = {},
+) => {
+  const [isTerminated, setIsTerminated] = useState(false)
+
+  const savedCallback = useRef(callback)
+  useIsomorphicEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (!when || isTerminated) return
+
+    const element = ref.current
+    if (!(element instanceof Element)) return
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0]
+        if (!entry) return
+
+        savedCallback.current(entry)
+        setIsTerminated(Boolean(entry.isIntersecting && once))
+      },
+      { root, rootMargin, threshold },
+    )
+
+    observer.observe(element)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [ref, when, isTerminated, once, root, rootMargin, threshold])
+}
