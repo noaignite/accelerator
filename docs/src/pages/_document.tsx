d0b151b002bf641db9a31910beee34a5b8b1d735
Martin Barri <maeertin@gmail.com>
2024-09-26 11:32:19 +0200

    feat(docs): add favicon, site logo and page title

--- docs/src/pages/_document.tsx
+++ docs/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image'
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Accelerator Docs</span>,
+  logo: (
+    <div style={{ display: 'flex', gap: 8 }}>
+      <Image alt="NoA Ignite" height={16} src="/icon.svg" width={16} />
+      <span>Accelerator Docs</span>
+    </div>
+  ),
   project: {
     link: 'https://github.com/noaignite/accelerator',
   },
@@ -9,6 +15,9 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Accelerator Docs',
   },
+  useNextSeoProps: () => ({
+    titleTemplate: '%s – Accelerator docs',
+  }),
 }
 
 export default config
