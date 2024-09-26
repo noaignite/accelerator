8046458672c1b076859352224477dac755018325
adamsoderstrom <adamsoderstrom@me.com>
2024-09-26 10:15:21 +0200

    chore(docs): remove `.generated` suffix in app

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,8 +1,52 @@
 import nextra from 'nextra'
 
+function traverseChildren(pageMapItem, callback) {
+  if (pageMapItem.children) {
+    pageMapItem.children.forEach((child) => {
+      traverseChildren(child, callback)
+    })
+  }
+
+  return callback(pageMapItem)
+}
+
+function formatGeneratedPageName(name) {
+  return name.replace(/\.generated/, '')
+}
+
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  transformPageOpts: (pageOpts) => {
+    const { pageMap } = pageOpts
+
+    pageOpts.pageMap = pageMap.flatMap((pageMapItem) => {
+      if (pageMapItem.kind === 'Folder' && pageMapItem.name === '@noaignite') {
+        traverseChildren(pageMapItem, (child) => {
+          if (child.kind === 'MdxPage' && child.name.endsWith('.generated')) {
+            child.name = formatGeneratedPageName(child.name)
+            console.log(child)
+          }
+
+          if (child.kind === 'Meta') {
+            child.data = Object.fromEntries(
+              Object.entries(([key, value]) => {
+                const formatted = formatGeneratedPageName(key)
+
+                return [formatted, formatted]
+              }),
+            )
+          }
+
+          return child
+        })
+      }
+
+      return [pageMapItem]
+    })
+
+    return pageOpts
+  },
 })
 
 export default withNextra()
