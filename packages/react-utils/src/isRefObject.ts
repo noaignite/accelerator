3054df194ccf6c849f628abd35132c10bec7ae48
Martin Barri <maeertin@gmail.com>
2024-10-04 14:34:01 +0200

    feat(react-utils): add `isRefObject` predicate & `useEvent` hook

---
+++ packages/react-utils/src/isRefObject.ts
@@ -0,0 +1,20 @@
+import { isObject } from '@noaignite/utils'
+import type { RefObject } from 'react'
+
+/**
+ * Checks if a given value is of type `RefObject`.
+ *
+ * @param value - The value to check.
+ * @returns `true` if the value is of type `RefObject`, `false` otherwise.
+ *
+ * @example
+ * ```ts
+ * const ref = useRef<HTMLDivElement>(null)
+ *
+ * isRefObject(ref) // true
+ * isRefObject({}) // false
+ * isRefObject(window) // false
+ * ```
+ */
+export const isRefObject = (value: unknown): value is RefObject<unknown> =>
+  isObject(value) && Object.isExtensible(value) && 'current' in value
