49d7a6b7a666699120c88099fab78a327bed2686
Martin Barri <maeertin@gmail.com>
2024-10-11 13:08:22 +0200

    feat(react-utils): add setRef helper

---
+++ packages/react-utils/src/setRef.test.ts
@@ -0,0 +1,39 @@
+import { createRef } from 'react'
+import { describe, expect, it, vi } from 'vitest'
+import { setRef } from './setRef'
+
+describe('setRef', () => {
+  it('can handle callback refs', () => {
+    const ref = vi.fn()
+    const instance = 'proxy'
+
+    setRef(ref, instance)
+
+    expect(ref).toHaveBeenCalled() // vitest matcher
+    expect(ref).toHaveBeenCalledWith(instance) // vitest matcher for args
+  })
+
+  it('can handle ref objects', () => {
+    const ref = createRef()
+    const instance = 'proxy'
+
+    setRef(ref, instance)
+
+    expect(ref.current).toBe(instance)
+  })
+
+  it('ignores falsy refs without errors', () => {
+    const instance = 'proxy'
+
+    // all no-ops
+    setRef(undefined, instance)
+    setRef(null, instance)
+  })
+
+  it('throws on legacy string refs', () => {
+    expect(() => {
+      // @ts-expect-error -- Test incorrect interface.
+      setRef('stringRef1', 'proxy')
+    }).toThrow()
+  })
+})
