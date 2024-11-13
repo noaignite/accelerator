fa7b2211a3ce9a05735500d1087e59b1f08f7a8f
Martin Barri <maeertin@gmail.com>
2024-11-13 14:47:12 +0100

    fix(prettier): update faulty config & format

--- packages/utils/src/omit.test.ts
+++ packages/utils/src/omit.test.ts
@@ -5,7 +5,9 @@ describe('omit', () => {
   it('returns the correct result', () => {
     expect(omit({ foo: 0, baz: 1 }, ['baz'])).toEqual({ foo: 0 })
     expect(omit({ foo: 0, baz: { bar: 1 } }, ['baz'])).toEqual({ foo: 0 })
-    expect(omit({ foo: 0, baz: { bar: 1 } }, ['foo'])).toEqual({ baz: { bar: 1 } })
+    expect(omit({ foo: 0, baz: { bar: 1 } }, ['foo'])).toEqual({
+      baz: { bar: 1 },
+    })
     // @ts-expect-error -- Test invalid keys
     expect(omit({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd'])).toEqual({ b: 2 })
   })
