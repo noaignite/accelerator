02c263fb868e4fc12fc3ad573f87809f2f43fac1
Dash <i@dashice.com>
2024-11-20 16:46:54 +0100

    test(react-utils): add test for  useInert

---
+++ packages/react-utils/src/useInert.test.tsx
@@ -0,0 +1,37 @@
+import { test } from 'vitest'
+
+test('applies inert to all elements except scope', async () => {
+  // no-op
+})
+
+test('does not apply inert when disabled', async () => {
+  // no-op
+})
+
+test('does not apply inert on a select few elements outside scope', async () => {
+  // no-op
+})
+
+test('applies and manages multiple `useInert` instance scopes', async () => {
+  // no-op
+})
+
+test('reverts DOM manipulations after all `useInert` instances are disabled', async () => {
+  // no-op
+})
+
+test('reverts subset of DOM manipulations when some `useInert` instances are disabled', async () => {
+  // no-op
+})
+
+test('with `useFocusReturn`, return focus to initiator', async () => {
+  // no-op
+})
+
+test('with `useFocusReturn`, return focus to correct initiator per `useInert` instance', async () => {
+  // no-op
+})
+
+test('on unmount, reverts DOM manipulations made by `useInert`', async () => {
+  // no-op
+})
