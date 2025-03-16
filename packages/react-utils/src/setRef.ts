86cde897db3166e76eaf85335885aec808edb553
Martin Barri <maeertin@gmail.com>
2025-03-16 11:35:06 +0100

    chore(react-utils/setRef): change out deprecated MutableRefObject

--- packages/react-utils/src/setRef.ts
+++ packages/react-utils/src/setRef.ts
@@ -1,3 +1,5 @@
+import type { RefObject } from 'react'
+
 /**
  * Useful if you want to expose the ref of an inner component to the public API
  * while still using it inside the component.
@@ -5,7 +7,7 @@
  * @param ref - A ref callback or ref object. If anything falsy, this is a no-op.
  */
 export function setRef<T>(
-  ref: React.MutableRefObject<T | null> | ((instance: T | null) => void) | null | undefined,
+  ref: RefObject<T | null> | ((instance: T | null) => void) | null | undefined,
   value: T | null,
 ): void {
   if (typeof ref === 'function') {
