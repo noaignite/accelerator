95869160ef58e37333b4142dbd4726928a4e62fb
Martin Barri <maeertin@gmail.com>
2025-03-16 14:20:56 +0100

    test(react-utils): add useElementSize tests

---
+++ packages/react-utils/test/IntersectionObserverMock.ts
@@ -0,0 +1,38 @@
+import { vi } from 'vitest'
+
+/**
+ * A simple IntersectionObserver mock that captures instances
+ * and exposes a .trigger() method to simulate intersections.
+ */
+export class IntersectionObserverMock {
+  private callback: IntersectionObserverCallback
+  private isDisconnected = false
+  public options: IntersectionObserverInit
+  public root: Element | Document | null
+  public rootMargin: string
+  public thresholds: number[]
+  static instances: IntersectionObserverMock[] = []
+
+  constructor(callback: IntersectionObserverCallback, options: IntersectionObserverInit = {}) {
+    const { root, rootMargin, threshold } = options
+
+    this.callback = callback
+    this.options = options
+    this.root = root ?? null
+    this.rootMargin = rootMargin ?? ''
+    this.thresholds = Array.isArray(threshold) ? threshold : [threshold ?? 0]
+
+    IntersectionObserverMock.instances.push(this)
+  }
+
+  observe = vi.fn()
+  unobserve = vi.fn()
+  takeRecords = vi.fn()
+  disconnect = vi.fn(() => {
+    this.isDisconnected = true
+  })
+  trigger = (entries: IntersectionObserverEntry[]) => {
+    if (this.isDisconnected) return
+    this.callback(entries, this)
+  }
+}
