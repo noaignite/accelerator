234db05930b9f577a0b63752a6f30c51562ec885
Martin Barri <maeertin@gmail.com>
2025-04-16 14:26:13 +0200

    chore: prettify & move away from .mjs

--- prettier.config.mjs
+++
@@ -1,5 +0,0 @@
-// WARN: Do not use one-liner export `export default '@repo/prettier-config'`
-// as it breaks the prettier configuration.
-import prettierConfig from '@noaignite/style-guide/prettier'
-
-export default prettierConfig
