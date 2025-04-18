2102b7355807db1f3ee006ec9ebe5d42e5bafda7
Martin Barri <maeertin@gmail.com>
2025-04-18 02:51:38 +0200

    fix(docs): add redirect from `/` to `/README`

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from 'next'
 import nextra, { type NextraConfig } from 'nextra'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/README',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 const nextraConfig: NextraConfig = {}
 
