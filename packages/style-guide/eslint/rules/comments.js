b5042e61ed27ef41bd195484dc3eeff55dbf877f
Martin Barri <maeertin@gmail.com>
2025-04-27 00:27:06 +0200

    feat(style-guide): add new import rules and add other rules documentation

--- packages/style-guide/eslint/rules/comments.js
+++ packages/style-guide/eslint/rules/comments.js
@@ -6,7 +6,18 @@ export default [
     name: '@noaignite/style-guide/comments',
     rules: {
       ...eslintConfigComments.recommended.rules,
+      /**
+       * This rule warns eslint-disable directive-comments if the eslint-enable
+       * directive-comment for that does not exist.
+       *
+       * 🚫 Not fixable - https://github.com/mysticatea/eslint-plugin-eslint-comments/blob/master/docs/rules/disable-enable-pair.md
+       */
       '@eslint-community/eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+      /**
+       * This rule warns directive comments without description.
+       *
+       * 🚫 Not fixable - https://github.com/mysticatea/eslint-plugin-eslint-comments/blob/master/docs/rules/require-description.md
+       */
       '@eslint-community/eslint-comments/require-description': 'error',
     },
   },
