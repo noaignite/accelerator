c765f40f2187b0b33eeed42ab4011357583c1c46
Dash <i@dashice.com>
2025-04-30 12:24:27 +0200

    feat(types): extend N with number[]

--- packages/types/src/Repeat.ts
+++ packages/types/src/Repeat.ts
@@ -1,12 +1,24 @@
 /**
  * Generates a tuple type of length `N` where each element is of type `T`.
+ * If `N` is `number[]`, it will generate a tuple for each number in the array.
  *
  * @example
  * ```tsx
- * type Test = Repeat<'foo', 3>;
+ * type Test = Repeat<'foo', 3>
  * // ['foo', 'foo', 'foo']
+ *
+ * type Test = Repeat<'foo', [1, 2]>
+ * // ['foo'] | ['foo', 'foo']
  * ```
  */
-export type Repeat<T, N extends number, R extends unknown[] = []> = R['length'] extends N
-  ? R
-  : Repeat<T, N, [T, ...R]>
+export type Repeat<T, N extends number | number[], R extends unknown[] = []> = N extends number[]
+  ? N[number] extends infer U
+    ? U extends number
+      ? Repeat<T, U>
+      : never
+    : never
+  : N extends number
+    ? R['length'] extends N
+      ? R
+      : Repeat<T, N, [T, ...R]>
+    : never
